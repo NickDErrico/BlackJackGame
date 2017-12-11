185918ec6734b46d6d4d210b80c7a39b8a0db214
NickDErrico <nickderrico@hotmail.com>
2017-12-11 13:43:26 -0700

    added some styling

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
 
+  $.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6', function(data) {
+  console.log(data['deck_id']);
+
+  let decks = data['deck_id'];
+
+  $('.deal').click(function(data) {
+    let dealCards = $.get('https://deckofcardsapi.com/api/deck/' + decks + '/draw/?count=1')
+    console.log(dealCards);
+
+  })
 
 
-
-  
+  })
 })
